51f47e088e76fb2bffdd98352cbdaa475d53feb9
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-05-25 14:23:03 +0200

    Created Home controller and finished logic for profile page

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,12 @@ router.get('/', (req, res) =>{
 })
 
 router.get('/profile/:account_id',(req, res) =>{
-    res.render('profile');
+    let id = req.params.account_id;
+    fs.readFile('data.json', 'utf-8', (err, content) =>{
+        let arrContent = JSON.parse(content);
+        let currentAcount = arrContent.find(user => user.id == id);
+        res.render('profile', {currentAcount: currentAcount, accounts: arrContent});
+    })
 })
 
 module.exports = router;
