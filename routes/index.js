b8d23d0842bffc9f1b4cecf62a33d39018ec1383
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-05-25 13:42:49 +0200

    Created profile.ejs and partial profile_info.ejs

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,11 @@ const router = expres.Router();
 router.get('/', (req, res) =>{
     fs.readFile('data.json', 'utf-8', (err, file) => {
         res.render('index', {accounts: JSON.parse(file)});
-    })
+    });
+})
+
+router.get('/profile/:account_id',(req, res) =>{
+    res.render('profile');
 })
 
 module.exports = router;
